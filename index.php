f9705b8b606eea2391e75f0e1ab199e48d31bcc7
Sara-Said <sara_said577@yahoo.com>
2014-03-14 12:04:32 +0200

    nothing

--- index.php
+++ index.php
@@ -14,14 +14,40 @@ and open the template in the editor.
         
         include 'lib.php';
         
-        $conn = createConnection("dbUser here ","dbPass Here");
-        $query = " Your Query Here " ;
         
-        mysqli_query($conn, $query);
-        
-        // don't forget to close connection :) 
-        mysqli_close($conn);
+         $conn = createConnection("root","654321");//1 connect to db
+         
+         $query = "select * from products where p_name ='".$_POST["name"]."'";
 
-        ?>
+          $result =mysqli_query($conn, $query);//3
+
+          $row = mysqli_fetch_array($result);
+           
+         drawEditLayout($row);//5 draw form to edit a product::::
+
+/*-----------------
+         $pname=$_POST["pname"];
+         $productList=array();
+         $count=0;
+          $query = "select * from products where p_name ='".$pname."'";
+          $result =mysqli_query($conn, $query);//3
+          while ($row = mysqli_fetch_array($result))
+            {
+              array_push($productList[$count],$row[$count]); //4 store query result in array ::::
+              $count++;
+            }
+//------------------*/
+
+          
+          
+                         
+    
+     //  don't forget to close connection :) 
+         mysqli_close($conn);  
+           
+      //   echo"insertion done";
+      ?>
     </body>
 </html>
+
+
--- lib.php
+++ lib.php
@@ -9,13 +9,16 @@
  * 
  * ----------> Name Convention <-----------
  */
-    function createConnection($dbUser,$dbPass) {
+    function createConnection($dbUser,$dbPass)//1
+    {
+
         $dbHost = "localhost";
         $dbName = "phpproject";
         $con = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
         
       // Check connection
-        if (mysqli_connect_errno()) {
+        if (mysqli_connect_errno())
+        {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
 
@@ -25,6 +28,72 @@
     // this is template .. add your implementation of your own function here :) 
     function yourFunction() {
         
+    }
+    //----------------------------------S.S
+       function  selectPToEdit()
+    {
+       echo"<html>";
+       echo"<body>";
+       echo"<form  action=index.php method=post>";
+       echo"Please Enter Product Name To Edit :";
+       echo"<INPUT TYPE=TEXT NAME=name   > <br> <br>";
+       echo"<input type=submit name=submitbtn value=EditProduct />";
+       echo"</form>";
+       echo"</body>";
+       echo"</html>";
+       
+
+   }
+    //---------------------------------S.S
+    function  drawEditLayout( $prodArr)
+    {
+       echo"<html>";
+       echo"<body>";
+       echo"<form  action=insertIntoDB.php  method=post>";
+      // echo"Product Id :";
+       echo"<INPUT NAME=pid type=hidden value=".$prodArr[0]." > <br> <br>";
+
+
+       echo"Product Name :";
+       echo"<INPUT TYPE=TEXT NAME=pname    value='".$prodArr[1]."' > <br> <br>";
+
+       echo"Product Description :";
+       //echo"<INPUT TYPE=TEXT NAME=pdesc    value=$prodArr[2]> <br> <br>";//
+       echo"<textarea rows=4 cols=50 name=pdesc value='".$prodArr[2]."'>  </textarea> <br> <br>";
+       echo"Product Price :";
+       echo"<INPUT TYPE=TEXT NAME=pprice   value=".$prodArr[3]." > <br> <br>";//
+       echo"Product Stock :";
+       echo"<INPUT TYPE=TEXT NAME=Pstock   value=".$prodArr[4]." > <br> <br>";//
+       echo"Product Image :";
+       echo"<INPUT TYPE=file NAME=pimg     value='".$prodArr[5]."' > <br> <br>"; //done
+       echo"Product QR :";
+       echo"<INPUT TYPE=TEXT NAME=Pqr      value='".$prodArr[6]."' > <br> <br>";
+       echo"Product Date:";
+       echo"<INPUT TYPE=TEXT NAME=pdate    value= '".$prodArr[7]."'> <br> <br>";//
+       echo"Product Category:";
+       echo"<INPUT TYPE=TEXT NAME=pcategory value=".$prodArr[8]."> <br> <br>";//
+       echo"<input type=submit name=submitbtn value=Done />";
+       echo"</form>";
+       echo"</body>";
+       echo"</html>";
+       
+    } 
+
+    
+
+    function deleteProducts()
+    {
+
+       echo"<html>";
+       echo"<body>";
+       echo"<form action=deleteproduct.php   method=post>";
+       echo"Please Enter Product Name To Delete :";
+       echo"<INPUT TYPE=TEXT NAME=pname  > <br> <br>";
+       echo"<input type=submit name=submitbtn value=DeleteProduct />";
+       echo"</form>";
+       echo"</body>";
+       echo"</html>";
+
     }
 
 ?>
